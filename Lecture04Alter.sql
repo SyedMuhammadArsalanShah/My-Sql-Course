78e0fa12e0960575a17ad4cf7f91f60d4a38adcf
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2024-05-19 17:59:34 +0500

    code updated Sql lecture03

---
+++ Lecture04Alter.sql
@@ -0,0 +1,23 @@
+
+
+
+select * from info;
+
+
+alter table info 
+add Address varchar(255),  add Age int;
+
+
+alter table info 
+drop Address ,  drop Contact;
+
+
+alter table info modify column Age varchar(255);
+
+
+
+
+
+
+
+
